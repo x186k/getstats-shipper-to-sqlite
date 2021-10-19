940760b115aa9c84b17d51c43736121312b90ade
Cameron Elliott <868689+cameronelliott@users.noreply.github.com>
2021-10-18 18:04:38 -0700

    run-time normalizing of json to DB. yuck?

--- main.go
+++ main.go
@@ -2,10 +2,11 @@ package main
 
 import (
 	"database/sql"
+	"io"
 	"io/ioutil"
 	"net/http"
+	"os"
 	"strings"
-	"sync"
 
 	"encoding/json"
 	"fmt"
@@ -34,38 +35,29 @@ func checkFatal(err error) {
 // so the javascript side of this, just makes a u64 bit random
 // https://github.com/w3c/webrtc-pc/issues/1775  Discussion on adding a unique id
 
-const createTable = `
-CREATE TABLE IF NOT EXISTS rawjson (
-	json TEXT NOT NULL,
-	updated DATETIME DEFAULT CURRENT_TIMESTAMP
-);
+const createAll = `
 CREATE TABLE IF NOT EXISTS getstats (
 	pcid INT8 NOT NULL,
 	updated DATETIME DEFAULT CURRENT_TIMESTAMP
 );
-`
-
-const createIndex = `
 CREATE INDEX IF NOT EXISTS getstats_pcid_idx ON getstats (pcid);
 CREATE INDEX IF NOT EXISTS getstats_updated_idx ON getstats (updated);
 `
 
 var cols map[string]struct{} = make(map[string]struct{})
-var colsMutex sync.Mutex
-
-const insert = `insert into getstats (pcid,json) values ($1,$2)`
 
 type BodyHalfDecode struct {
 	PCID    string
 	Reports map[string]json.RawMessage
 }
 
-type BodyFullDecode struct {
-	PCID    string
-	Reports map[string]map[string]interface{}
-}
+// type BodyFullDecode struct {
+// 	PCID    string
+// 	Reports map[string]map[string]interface{}
+// }
 
 var hostport = pflag.String("hostport", ":8080", "host:port for http")
+var debug = pflag.BoolP("debug", "d", false, "enable debug log")
 var saveJson = pflag.BoolP("save-json", "j", false, "Enables the saving of raw json")
 var saveNormalized = pflag.BoolP("save-normalized", "n", true, "Enables the saving of normalized data: no json_extract(...) for reports")
 
@@ -78,19 +70,25 @@ var db *sqlx.DB
 
 var bodyChan = make(chan []byte)
 
+var dbg = log.New(os.Stdout, "D ", log.LstdFlags|log.Lshortfile)
+
 func main() {
 	var err error
 
 	log.SetFlags(log.LstdFlags | log.Lshortfile)
 	pflag.Parse()
 
-	db, err = sqlx.Open("sqlite3", "getstats.db")
+	if !*debug {
+		dbg.SetOutput(io.Discard)
+	}
+
+	// https://github.com/mattn/go-sqlite3
+	db, err = sqlx.Open("sqlite3", "getstats.db?cache=shared&mode=rwc&_journal_mode=WAL")
 	checkFatal(err)
 
 	db.SetMaxOpenConns(5)
 
-	db.MustExec(createTable)
-	db.MustExec(createIndex)
+	db.MustExec(createAll)
 
 	rows, err := db.Query("PRAGMA table_info(getstats)")
 	checkFatal(err)
@@ -112,98 +110,121 @@ func main() {
 	mux := http.NewServeMux()
 	mux.HandleFunc("/", postHandler)
 
-	go func() {}()
-	err = http.ListenAndServe(*hostport, mux)
+	go func() {
+		err = http.ListenAndServe(*hostport, mux)
+		checkFatal(err)
+	}()
+
+	lastBody, err = os.Create("jsonlog.txt")
 	checkFatal(err)
+
+	n := 0
 
 	for body := range bodyChan {
 
-		if *saveJson {
-			n, err := db.MustExec(insert, decodedBody.PCID, string(v)).RowsAffected()
-			if err != nil {
-				http.Error(w, err.Error(), http.StatusInternalServerError)
-				log.Println(err.Error())
-				return
+		if n >= 1000 {
+			off, err := lastBody.Seek(0, io.SeekStart)
+			checkFatal(err)
+			if off != 0 {
+				checkFatal(fmt.Errorf("bad seek off"))
 			}
+			err = lastBody.Truncate(0)
+			checkFatal(err)
+
+			_, err = lastBody.Write(body)
+			checkFatal(err)
+			_, err = lastBody.WriteString("\n")
+			checkFatal(err)
+
+			n = 0
 		}
 
 		if *saveNormalized {
-			var x BodyFullDecode
-			err = json.Unmarshal(body, &x)
-			if err != nil {
-				log.Println(err.Error())
-				return
-			}
-
 			// {"id":"RTCCodec_0_Inbound_107","timestamp":1634435347027.614,"type":"codec","transportId":"RTCTransport_0_1","payloadType":107,"mimeType":"video/rtx","clockRate":90000,"sdpFmtpLine":"apt=125"}
 			// {"id":"RTCCodec_0_Inbound_124","timestamp":1634435347027.614,"type":"codec","transportId":"RTCTransport_0_1","payloadType":124,"mimeType":"video/H264","clockRate":90000,"sdpFmtpLine":"level-asymmetry-allowed=1;packetization-mode=1;profile-level-id=4d0032"}
 			// {"id":"RTCCodec_0_Outbound_109","timestamp":1634435347027.614,"type":"codec","transportId":"RTCTransport_0_1","payloadType":109,"mimeType":"video/rtx","clockRate":90000,"sdpFmtpLine":"apt=108"}
-
-			for _, singleReport := range x.Reports {
-
-				keys := make([]string, len(singleReport))
-				qmarks := make([]string, len(singleReport))
-				vals := make([]interface{}, len(singleReport))
-
-				i := 0
-				for key, val := range singleReport {
-
-					valarray, isarray := val.([]interface{})
-					if isarray {
-						val = StrConvert(valarray)
-					}
-					vals[i] = fmt.Sprintf("%v", val) //convert to string
-					key = strings.Map(safechar, key)
-					keys[i] = key
-					qmarks[i] = "?"
-
-					i++
-
-					colsMutex.Lock()
-					_, ok := cols[key]
-					colsMutex.Unlock()
-
-					if !ok {
-						valtype := ""
-						println(777, key, val)
-						err = db.Get(&valtype, "SELECT typeof(?)", val)
-						checkFatal(err)
-
-						println(88, valtype)
-
-						const addcol = `ALTER TABLE getstats ADD COLUMN %s %s`
-
-						colsMutex.Lock()
-						cols[key] = struct{}{}
-						colsMutex.Unlock()
-						sqlx.MustExec(db, fmt.Sprintf(addcol, key, valtype))
-					}
-				}
-
-				insertstmt := fmt.Sprintf("insert into getstats (pcid,%s) values (%s,%s)", strings.Join(keys, ","), x.PCID, strings.Join(qmarks, ","))
-				println(33, insertstmt)
-				println(44, StrConvert(vals), ",")
-
-				db.MustExec(insertstmt, vals...)
-
-				// n, err := db.MustExec(insert, x.PCID, string(singleReport)).RowsAffected()
-				// if err != nil {
-				// 	http.Error(w, err.Error(), http.StatusInternalServerError)
-				// 	log.Println(err.Error())
-				// 	return
-				// }
-				// if n != 1 {
-				// 	m := fmt.Errorf("insert did not return one: %d", n)
-				// 	http.Error(w, m.Error(), http.StatusInternalServerError)
-				// 	log.Println(m.Error())
-				// 	return
-				// }
-
+			//convert to string
+			dbg.Println("calling insertIntoDB")
+			err := insertIntoDB(body)
+			if err != nil {
+				log.Println(fmt.Errorf("err:%w  body:%s", err, string(body)))
 			}
-
 		}
 	}
 
+}
+
+var lastBody *os.File
+
+// var lastRow *os.File
+
+func insertIntoDB(body []byte) error {
+	var err error
+
+	var x BodyHalfDecode
+	err = json.Unmarshal(body, &x)
+	if err != nil {
+		return err
+	}
+
+	for _, row := range x.Reports {
+
+		var singleReport map[string]interface{}
+
+		err = json.Unmarshal(row, &singleReport)
+		if err != nil {
+			return err
+		}
+
+		keys := make([]string, len(singleReport))
+		qmarks := make([]string, len(singleReport))
+		vals := make([]interface{}, len(singleReport))
+
+		i := 0
+		for key, val := range singleReport {
+
+			valarray, isarray := val.([]interface{})
+			if isarray {
+				val = StrConvert(valarray)
+			}
+			vals[i] = fmt.Sprintf("%v", val)
+			key = strings.Map(safechar, key)
+			keys[i] = key
+			qmarks[i] = "?"
+
+			i++
+
+			_, ok := cols[key]
+
+			if !ok {
+				valtype := ""
+
+				err = db.Get(&valtype, "SELECT typeof(?)", val)
+				if err != nil {
+					return fmt.Errorf("select typeof fail: %w, json: %s", err, string(row))
+				}
+
+				cols[key] = struct{}{}
+
+				addcol := fmt.Sprintf(`ALTER TABLE getstats ADD COLUMN %s %s`, key, valtype)
+				_, err := db.Exec(addcol)
+				if err != nil {
+					return fmt.Errorf("alter table fail: %w, json: %s, sql: %s", err, string(row), addcol)
+				}
+			}
+		}
+
+		insertstmt := fmt.Sprintf("insert into getstats (pcid,%s) values (%s,%s)", strings.Join(keys, ","), x.PCID, strings.Join(qmarks, ","))
+		dbg.Println(insertstmt)
+		dbg.Println("values = ", StrConvert(vals))
+
+		_, err := db.Exec(insertstmt, vals...)
+		if err != nil {
+			return fmt.Errorf("insert into table fail: %w, json: %s, sql: %s", err, string(row), insertstmt)
+		}
+
+	}
+	return nil
 }
 func safechar(r rune) rune {
 	switch {
@@ -229,8 +250,6 @@ func StrConvert(a ...interface{}) string {
 	return str
 }
 
-
-
 func postHandler(w http.ResponseWriter, r *http.Request) {
 
 	body, err := ioutil.ReadAll(r.Body)
@@ -240,37 +259,8 @@ func postHandler(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	bodyChan <- string(body)
-
-}
-
-var _ = saveJsonFunc
-
-func saveJsonFunc(body []byte, w http.ResponseWriter) {
-	var decodedBody BodyHalfDecode
-	var err error
-	err = json.Unmarshal(body, &decodedBody)
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		log.Println(err.Error())
-		return
-	}
-
-	for _, v := range decodedBody.Reports {
-
-		n, err := db.MustExec(insert, decodedBody.PCID, string(v)).RowsAffected()
-		if err != nil {
-			http.Error(w, err.Error(), http.StatusInternalServerError)
-			log.Println(err.Error())
-			return
-		}
-		if n != 1 {
-			m := fmt.Errorf("insert did not return one: %d", n)
-			http.Error(w, m.Error(), http.StatusInternalServerError)
-			log.Println(m.Error())
-			return
-		}
-
-	}
+	println(88)
+	bodyChan <- body
+	println(77)
 
 }
